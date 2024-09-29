9dca96dffffc5ddb38f157a054743acb260d4dee
sumondas2001 <sumonchandradas1010@gmail.com>
2024-09-29 17:47:01 +0600

    all install  and all setup

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DV_USER}:${process.env.DV_PASS}@cluster0.3y9ux.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+     serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+     }
+});
+
+async function run() {
+     try {
+          // Connect the client to the server	(optional starting in v4.7)
+          await client.connect();
+
+
+
+
+
+
+          // Send a ping to confirm a successful connection
+          await client.db("admin").command({ ping: 1 });
+          console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     } finally {
+          // Ensures that the client will close when you finish/error
+          // await client.close();
+     }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+     res.send('Coffee making server is running')
+});
+
+app.listen(port, () => {
+     console.log(`coffee server running on port : ${port}`)
+})
